4ce9f4c91ca4dc9afcd6f68c7166642f2edcfac9
SherlockH0 <ddmlf7@gmail.com>
2025-05-19 09:41:12 +0300

    fix treesitter

--- .config/nvim/lua/sherlock/plugins/treesitter.lua
+++ .config/nvim/lua/sherlock/plugins/treesitter.lua
@@ -1,13 +1,21 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
-	opts = {
-		ensure_installed = { "javascript", "python", "c", "lua", "vim", "vimdoc", "query" },
-		sync_install = false,
-		auto_install = true,
-		highlight = {
-			enable = true,
-			additional_vim_regex_highlighting = false,
-		},
-	},
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			modules = {},
+			ignore_install = {},
+			ensure_installed = { "javascript", "python", "c", "lua", "vim", "vimdoc", "query" },
+
+			sync_install = false,
+
+			auto_install = true,
+
+			indent = { enable = true },
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+		})
+	end,
 }
